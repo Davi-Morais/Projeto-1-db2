aeb01963b1fe33261e57dd6d45f1eab2f8efec59
Davi Morais <davimorais847@gmail.com>
2023-01-16 20:46:14 -0300

    estilo do arquivo all_hemonucleos.html concluida

--- assets/javascript/all_hemonucleos.js
+++ assets/javascript/all_hemonucleos.js
@@ -4,15 +4,16 @@ let marker;
 let center = {lat: -6.888463202449027, lng: -38.558930105104125};
 
 function loadAll() {
+    map = new google.maps.Map(document.getElementById("map"), {
+        center: center,
+        zoom: 14,
+    });
+
     const dados = fetch('http://localhost:3000/pontos', {
         method: 'GET',
     }).then(res => res.json());
 
     dados.then( d => {
-        map = new google.maps.Map(document.getElementById("map"), {
-            center: center,
-            zoom: 14,
-          });
 
         d.forEach(element => {
             marker = new google.maps.Marker({
